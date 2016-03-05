d7f1db1beec6a86413064a7d4c35ab6445587e36
Daniel Cohen <dan@supercore.co.uk>
2016-03-05 19:42:16 +0000

    added base guestbook message leaving

--- functs.js
+++ functs.js
@@ -1,3 +1,20 @@
 function postM() {
-  alert("that worked");
+  var tempName = document.getElementById("formName").value;
+  var tempAge = document.getElementById("formAge").value;
+  var tempGender = document.getElementById("formGender").value;
+  var tempLoc = document.getElementById("formLoc").value;
+  var tempMess = document.getElementById("formMess").value;
+  var messagenode = document.createElement("p");
+  var personnode = document.createElement("p");
+  var textnodeM = document.createTextNode(tempMess);
+  var fullPerson = document.createTextNode(tempName + ", " + tempAge + ", " + tempGender + ", " + tempLoc);
+  var nodebr = document.createElement("br");
+  messagenode.appendChild(textnodeM);
+
+  personnode.appendChild(fullPerson);
+
+  document.getElementById("guestmessages").appendChild(messagenode);
+  document.getElementById("guestmessages").appendChild(personnode);
+
+  return false;
 }
